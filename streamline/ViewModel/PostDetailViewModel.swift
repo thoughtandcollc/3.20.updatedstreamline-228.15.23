c4e9a6a63b0a3d82f260157b959fe5490fc0057b
Sibtain Ali <sibtain@designakin.com>
2023-04-28 18:24:34 +0500

    delete post functionality

--- streamline/ViewModel/PostDetailViewModel.swift
+++ streamline/ViewModel/PostDetailViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 class PostDetailViewModel: ObservableObject {
     
@@ -19,25 +20,72 @@ extension PostDetailViewModel {
     
     func deletePost(post: Post, completion: @escaping (_ success: Bool)->()) {
         
-        customAlertApple(title: "Delete Post", message: "Are you sure you want to delete this post?", showDestructive: true) { success in
+        customAlertApple(title: "Delete Post", message: "Are you sure you want to delete this post?", showDestructive: true) {[weak self] success in
+            guard let self = self else { return }
             guard success else { completion(false); return }
             
-            COLLECTION_POSTS.document(post.id).delete { error in
+            // delete photos first
+            self.deletePhotosFromFirebase(post: post) { success in
                 
-                // error
-                guard error == nil else {
-                    customAlert(message: error!.localizedDescription)
-                    completion(false)
-                    return
+                guard success else { return }
+                
+                // then delete post
+                COLLECTION_POSTS.document(post.id).delete { error in
+                    
+                    // error
+                    guard error == nil else {
+                        customAlert(message: error!.localizedDescription)
+                        completion(false)
+                        return
+                    }
+                    
+                    // success
+                    customAlert(message: "Post successfully deleted")
+                    
+                    completion(true)
+                    
                 }
-                
-                // success
-                customAlert(message: "Post successfully deleted")
-                
-                completion(true)
                 
             }
             
+        }
+        
+    }
+    
+    private func deletePhotosFromFirebase(post: Post, completion: @escaping (_ success: Bool)-> Void) {
+        
+        // check media count
+        guard post.media.count > 0 else { completion(true); return }
+        
+        let urls = post.media.map({ $0.imageURL })
+        
+        // for waiting till all images are deleted
+        let manager = DispatchGroup()
+        
+        // delete all images one by one
+        for url in urls {
+            
+            // enter queue
+            manager.enter()
+            
+            // get firebase reference using the urls
+            let ref = Storage.storage().reference(forURL: url)
+            ref.delete { error in
+                
+                // check for error
+                if error != nil {
+                    print("error deleting post image: \(error!.localizedDescription)")
+                }
+                
+                // leave queue
+                manager.leave()
+                
+            }
+        }
+        
+        // call completion once all image are deleted
+        manager.notify(queue: .main) {
+            completion(true)
         }
         
     }
